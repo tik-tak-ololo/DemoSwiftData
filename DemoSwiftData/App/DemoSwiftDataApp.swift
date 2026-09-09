b82472377cfda6104a33a2cc63a98dddebc4726b
Sergey Khmelow <tik-tak-ololo@outlook.com>
2026-09-10 02:08:34 +0700

    initial commit

---
+++ DemoSwiftData/App/DemoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftDataApp.swift
+//  DemoSwiftData
+//
+//  Created by Сергей Хмелёв on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoSwiftData/Screens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoSwiftData
+//
+//  Created by Сергей Хмелёв on 10.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
